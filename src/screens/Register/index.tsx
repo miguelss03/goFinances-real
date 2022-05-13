788d59463a0803c74990989ebf5e852c1eca6556
Miguel S. Silva <miguelprogs.silva@gmail.com>
2022-05-13 00:14:35 -0300

    Meu ovo

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import {
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionTypes
+} from './styles';
+import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button/index';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
+
+export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypesSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder='Nome'
+                    />
+
+                    <Input
+                        placeholder='Preço'
+                    />
+
+                    <TransactionTypes>
+                        <TransactionTypeButton
+                            type="up"
+                            title="Entradas"
+                            onPress={() => handleTransactionTypesSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton
+                            type="down"
+                            title="Saídas"
+                            onPress={() => handleTransactionTypesSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionTypes>
+
+                </Fields>
+                <Button
+                    title='Enviar'
+                />
+            </Form>
+        </Container>
+    );
+}
